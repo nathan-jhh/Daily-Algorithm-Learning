0f0bd5cbb866d4a2d1d4d0e9273777acaa34f470
Nathan-jiang <53858898+nathan-jhh@users.noreply.github.com>
2023-08-11 22:20:06 +0800

    删除链表 M 个节点之后的 N 个节点

---
+++ src/linkedlist/DeleteNodesFromLinkedList.java
@@ -0,0 +1,102 @@
+package linkedlist;
+
+/**
+ * 1474. 删除链表 M 个节点之后的 N 个节点
+ * 给定链表 head 和两个整数 m 和 n. 遍历该链表并按照如下方式删除节点:
+ * 开始时以头节点作为当前节点.
+ * 保留以当前节点开始的前 m 个节点.
+ * 删除接下来的 n 个节点.
+ * 重复步骤 2 和 3, 直到到达链表结尾.
+ * 在删除了指定结点之后, 返回修改过后的链表的头节点.
+ * https://leetcode.cn/problems/delete-n-nodes-after-m-nodes-of-a-linked-list/?
+ * envType=study-plan-v2&envId=premium-algo-100
+ */
+public class DeleteNodesFromLinkedList {
+    public static void main(String[] args) {
+        /*int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        ListNode listNode = new ListNode(nums);
+        System.out.println("listNode = " + listNode);
+
+        ListNode newListNode = new Solution().deleteNodes(listNode, 1, 3);
+        System.out.println("newListNode = " + newListNode);*/
+
+        int[] nums2 = new int[]{6, 3, 5, 6, 2, 8, 9, 2, 3, 4};
+        ListNode listNode2 = new ListNode(nums2);
+        System.out.println("listNode2 = " + listNode2);
+
+        ListNode newListNode2 = new Solution().deleteNodes(listNode2, 2, 1);
+        System.out.println("newListNode2 = " + newListNode2);
+    }
+
+    // 递归
+    static class Solution {
+        public ListNode deleteNodes(ListNode head, int m, int n) {
+            if (head == null) {
+                return null;
+            }
+
+            // 第m个节点
+            ListNode mNode = head;
+            // 第n+m+1个节点
+            ListNode mnNode = head;
+            for (int i = 0; i < m - 1; i++) {
+                if (mNode.next == null) {
+                    break;
+                }
+                mNode = mNode.next;
+            }
+            for (int i = 0; i < m + n; i++) {
+                mnNode = mnNode.next;
+                if (mnNode == null) {
+                    break;
+                }
+            }
+
+            mNode.next = deleteNodes(mnNode, m, n);
+            return head;
+        }
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        // 使用arr作为参数，创建一个链表，当前this链表为链表头节点
+        public ListNode(int arr[]) {
+            if (arr == null || arr.length == 0) {
+                throw new IllegalArgumentException("arr can not be empty");
+            }
+
+            this.val = arr[0];
+            ListNode cur = this;
+            for (int i = 1; i < arr.length; i++) {
+                cur.next = new ListNode(arr[i]);
+                cur = cur.next;
+            }
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder res = new StringBuilder();
+            ListNode cur = this;
+            while (cur != null) {
+                res.append(cur.val + "->");
+                cur = cur.next;
+            }
+            res.append("null");
+            return res.toString();
+        }
+    }
+}
