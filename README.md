97ae81567d805df124e96352b40cb318cc599627
Nathan-jiang <53858898+nathan-jhh@users.noreply.github.com>
2023-08-17 19:04:31 +0800

    判断环形链表

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 | [1474. 删除链表 M 个节点之后的 N 个节点](https://leetcode.cn/problems/delete-n-nodes-after-m-nodes-of-a-linked-list/?envType=study-plan-v2&envId=premium-algo-100) | easy | [Java](src/linkedlist/DeleteNodesFromLinkedList.java) |
 | [206. 反转链表](https://leetcode.cn/problems/reverse-linked-list/) | easy | [Java](src/linkedlist/ReverseLinkedList.java) |
 | [92. 反转链表 II](https://leetcode.cn/problems/reverse-linked-list-ii/description/) | medium | [Java](src/linkedlist/ReverseLinkedListII.java) |
+| [141. 环形链表](https://leetcode.cn/problems/linked-list-cycle/description/) | easy | [Java](src/linkedlist/LinkedListCycle.java) |
 
 ## 二叉树BinaryTree
 | 题目 | 难度 | 解答 |
---
+++ src/linkedlist/LinkedListCycle.java
@@ -0,0 +1,45 @@
+package linkedlist;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 给你一个链表的头节点 head ，判断链表中是否有环。
+ * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。
+ * 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。
+ * 注意：pos 不作为参数进行传递 。仅仅是为了标识链表的实际情况。
+ * 如果链表中存在环 ，则返回 true 。 否则，返回 false 。
+ * https://leetcode.cn/problems/linked-list-cycle/description/
+ */
+public class LinkedListCycle {
+    // hash table
+    public class Solution {
+        public boolean hasCycle(ListNode head) {
+            Set<ListNode> seen = new HashSet<ListNode>();
+            while (head != null) {
+                if (!seen.add(head)) {
+                    return true;
+                }
+                head = head.next;
+            }
+            return false;
+        }
+    }
+
+    // slow-fast pointer
+    public class Solution2 {
+        public boolean hasCycle(ListNode head) {
+            if (head == null || head.next == null) {
+                return false;
+            }
+            ListNode slow = head;
+            ListNode fast = head;
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+                if (slow == fast) return true;
+            }
+            return false;
+        }
+    }
+}
