582071e05258680bf379ada85b73cd528954eb85
Nathan-jiang <53858898+nathan-jhh@users.noreply.github.com>
2023-08-29 22:39:38 +0800

    86. 分隔链表

--- README.md
+++ README.md
@@ -11,6 +11,7 @@
 | [142. 环形链表 II](https://leetcode.cn/problems/linked-list-cycle-ii/) | medium | [Java](src/linkedlist/LinkedListCycleII.java) |
 | [19. 删除链表的倒数第 N 个结点](https://leetcode.cn/problems/remove-nth-node-from-end-of-list/?envType=study-plan-v2&envId=top-interview-150) | medium | [Java](src/linkedlist/RemoveNthNodeFromEndOfList.java) |
 | [2. 两数相加](https://leetcode.cn/problems/add-two-numbers/description/?envType=study-plan-v2&envId=top-interview-150) | medium | [Java](src/linkedlist/AddTwoNumbers.java) |
+| [86. 分隔链表](https://leetcode.cn/problems/partition-list/) | medium | [Java](src/linkedlist/PartitionList.java) |
 
 ## 二叉树BinaryTree
 | 题目 | 难度 | 解答 |
---
+++ src/linkedlist/PartitionList.java
@@ -0,0 +1,47 @@
+package linkedlist;
+
+/**
+ * 86. 分隔链表
+ * 给你一个链表的头节点 head 和一个特定值 x ，请你对链表进行分隔，使得所有 小于 x 的节点都出现在 大于或等于 x 的节点之前。
+ * 你应当 保留 两个分区中每个节点的初始相对位置。
+ * https://leetcode.cn/problems/partition-list/
+ */
+public class PartitionList {
+
+    // recursion
+    class Solution {
+        public ListNode partition(ListNode head, int x) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+
+            ListNode node = partition(head.next, x);
+            if (head.val < x) {
+                head.next = node;
+                return head;
+            } else {
+                // 可能head之后并没有小于x的节点，这种情况不用动，直接返回head
+                if (node.val > x) {
+                    head.next = node;
+                    return head;
+                }
+
+                ListNode dummy = new ListNode(-1, node);
+                ListNode cur = dummy;
+                while (cur.next != null && cur.next.val < x) {
+                    cur = cur.next;
+                }
+                head.next = cur.next;
+                cur.next = head;
+                return dummy.next;
+            }
+        }
+    }
+
+    // loop
+    class Solution2 {
+        public ListNode partition(ListNode head, int x) {
+
+        }
+    }
+}
